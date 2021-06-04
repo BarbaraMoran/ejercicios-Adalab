14cabb35f47e7385e8c575c6d5a6c2ebbf8cf535
BarbaraMoran <barbaramoranrr@gmail.com>
2021-06-04 10:59:01 +0200

    add exercise 5

--- Modulo-3/modulo 3-leccion05- ej5/src/components/SheepCounter.js
+++ Modulo-3/modulo 3-leccion05- ej5/src/components/SheepCounter.js
@@ -6,42 +6,36 @@ class SheepCounter extends React.Component {
     super(props);
     this.state = {
       number: 0,
-      img: "",
     };
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick() {
     this.setState((prevState, props) => {
-      let newNumber;
-      if (prevState.number === 0) {
-        newNumber = prevState.number + 1;
-      } else {
-        newNumber = prevState.number + 1;
-      }
-      let moreImg;
-      if (prevState.img === "") {
-        moreImg = (
-          <img src="https://lh3.googleusercontent.com/proxy/KHysHGytE0HlSB6_j1Eu6hYZdkE6L9GyS3v8i38D2JjQTwLpGbd2MxmZiJEMnAv47tu_kj3SszD4wgikGP1IXWVGldgnUHg" />
-        );
-        //no sirve, incompleto :(
-      } else {
-        moreImg = prevState.img;
-      }
+      let pepino = prevState.number + 1;
 
       return {
-        number: newNumber,
-        img: moreImg,
+        number: pepino,
       };
     });
   }
 
   render() {
+    const sheeps = [];
+    for (let i = 0; i < this.state.number; i++) {
+      sheeps.push(
+        <img
+          width="35px"
+          src="http://www.clker.com/cliparts/e/4/8/7/13280460782141411990Cartoon%20Sheep.svg.hi.png"
+          alt="sheep-cartoon"
+        />
+      );
+    }
     return (
       <>
         <input type="button" value="CuentaOvejas" onClick={this.handleClick} />
         <h1>{this.state.number}</h1>
-        {this.state.img}
+        {sheeps}
       </>
     );
   }
